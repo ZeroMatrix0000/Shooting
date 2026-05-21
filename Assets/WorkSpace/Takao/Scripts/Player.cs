16870d9890d6ad895d6663e0e877d0af8498313d
TSITCL\it252120 <it252120@trident.ac.jp>
2026-05-21 19:01:53 +0900

    Player移動実装

--- Assets/WorkSpace/Takao/Scripts/Player.cs
+++ Assets/WorkSpace/Takao/Scripts/Player.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
-
-
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+
+    /* インスペクター */
+
+    [SerializeField] private float speed;
+
+
+    /* メンバ変数 */
+
+
+    /* メンバ関数 */
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +23,35 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 direction = Vector2.zero;
 
+        if (Keyboard.current.upArrowKey.isPressed || Keyboard.current.wKey.isPressed)
+        {
+            direction += Vector2.up;
+        }
+        if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed)
+        {
+            direction += Vector2.left;
+        }
+        if (Keyboard.current.downArrowKey.isPressed || Keyboard.current.sKey.isPressed)
+        {
+            direction += Vector2.down;
+        }
+        if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed)
+        {
+            direction += Vector2.right;
+        }
+
+        if (direction.magnitude < 1e-6f)
+        {
+            direction = Vector2.zero;
+        }
+        else
+        {
+            direction.Normalize();
+        }
+
+        transform.localPosition += (Vector3)direction * speed * Time.deltaTime;
     }
+
 }
